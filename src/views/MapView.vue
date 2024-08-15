26504bc5551ede9eec5ba685aa7b3cc9a561efe1
Yurii Hupalo <uragupalo@gmail.com>
2024-08-15 17:18:58 +0200

    Render example GeoJson collection

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -6,21 +6,27 @@ import { useHead } from '@unhead/vue'
 import { onMounted } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
+import { exampleCollection } from '../assets/exampleGeoJson'
+import { center } from '@turf/center'
 
 useHead({
   title: 'Yurii'
 })
 
 onMounted(() => {
-  const map = L.map('map').setView([51.505, -0.09], 13)
+  const routeCenter = center(exampleCollection).geometry.coordinates.reverse() as [number, number]
+  const map = L.map('map', { center: routeCenter, zoom: 1 })
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map)
+  const routeLayer = L.geoJSON(exampleCollection).addTo(map)
+  const routeBounds = routeLayer.getBounds()
+  map.fitBounds(routeBounds)
 })
 </script>
 <style scoped lang="scss">
 #map {
-  height: 380px;
+  height: 480px;
 }
 </style>
