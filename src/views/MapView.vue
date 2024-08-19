e4d79b6a76ae103f8beadf95f2fb0b9636215553
Yurii Hupalo <uragupalo@gmail.com>
2024-08-19 10:50:00 +0200

    Add circle marker on hover

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,13 +4,27 @@
 <script setup lang="ts">
 import { useHead } from '@unhead/vue'
 import { onMounted } from 'vue'
-import L from 'leaflet'
+import L, { LatLng } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { exampleCollection } from '../assets/exampleGeoJson'
 
 useHead({
   title: 'Yurii'
 })
+
+function findHoveredCoordIndex(coords: [number, number, number][], target: LatLng) {
+  let minDistance = Infinity
+  let closestIndex = -1
+  coords.forEach((c, index) => {
+    const coordLatLng = L.latLng(c[1], c[0])
+    const distance = target.distanceTo(coordLatLng)
+    if (distance < minDistance) {
+      minDistance = distance
+      closestIndex = index
+    }
+  })
+  return closestIndex
+}
 
 onMounted(() => {
   const routeLayer = L.geoJSON(exampleCollection)
@@ -22,11 +36,29 @@ onMounted(() => {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map)
   routeLayer.addTo(map)
+  const circleMarker = L.circleMarker([0, 0], {
+    radius: 7,
+    color: 'red',
+    fillOpacity: 0.5,
+    className: 'hover-indicator'
+  })
+  routeLayer.on('mousemove', ({ propagatedFrom, latlng }) => {
+    const coords = propagatedFrom.feature.geometry.coordinates
+    const hoveredIndex = findHoveredCoordIndex(coords, latlng)
+    circleMarker.setLatLng(L.latLng(coords[hoveredIndex][1], coords[hoveredIndex][0]))
+    map.addLayer(circleMarker)
+  })
+  routeLayer.on('mouseout', () => {
+    map.removeLayer(circleMarker)
+  })
   map.fitBounds(routeBounds)
 })
 </script>
 <style scoped lang="scss">
 #map {
   height: 480px;
+}
+:deep(.hover-indicator) {
+  pointer-events: none !important;
 }
 </style>
